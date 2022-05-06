ae46557f49e702f3a2ec69819dc29482de900959
ASHOK KUMAR <96563901+ASHOK44321@users.noreply.github.com>
2022-05-06 15:37:01 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=n;i!=0;i--)     {         for(j=n;j!=0;j--)         {             printf("%d ",j);
            }
            printf("
    ");
            }
            return 0;
        
    }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,20 +1,18 @@
 #include<stdio.h>
 int main()
-{ 
-    int i,j,n;
+{
+    int n,i,j;
     scanf("%d",&n);
-    for(i=0;i<n;i++)
+    for(i=n;i!=0;i--)
     {
-        for(j=0;j<n;j++)
+        for(j=n;j!=0;j--)
         {
-            if(i==j)
-            printf("0");
-            if(i!=j)
-            printf("x");
-            }
-            printf("
-"); 
-        
-    }
+            printf("%d ",j);
+            
+        }
+        printf("
+");
+        }
+        return 0;
     
 }
