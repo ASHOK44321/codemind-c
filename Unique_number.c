bc3490ed6f24a45f7b348bdbe8a6788c9cfefdae
ASHOK KUMAR <96563901+ASHOK44321@users.noreply.github.com>
2022-05-06 15:27:41 +0530

    #include<stdio.h> int main() {     int n,i=0,j,k,t,s,c,z=0;     int a[100];     scanf("%d",&n);     while(n!=0)     {         t=n%10;         a[i]=t;         n=n/10;         i++;     }
            for(j=0;j<i;j++)
            {
        
                for(k=0;k<i;k++) 
                {
                  
                    if(k!=j)
                    {
                        if(a[j]==a[k]) 
                        {
                            c++;
                        }
                    }
                }
            }
    
                            if(c==0)
                            {
                            printf("Unique Number");
                            }
                            else 
                            {
                            printf("Not Unique Number");
                            }
    }

---
+++ Unique_number.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    int n,i=0,j,k,t,s,c,z=0; 
+    int a[100];
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        t=n%10;
+        a[i]=t; 
+        n=n/10;
+        i++;
+    }
+
+        for(j=0;j<i;j++)
+        {
+    
+            for(k=0;k<i;k++) 
+            {
+              
+                if(k!=j)
+                {
+                    if(a[j]==a[k]) 
+                    {
+                        c++;
+                    }
+                }
+            }
+        }
+
+                        if(c==0)
+                        {
+                        printf("Unique Number");
+                        }
+                        else 
+                        {
+                        printf("Not Unique Number");
+                        }
+}
