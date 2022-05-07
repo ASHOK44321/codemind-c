0fb0e8a09a75a9d1d52047df33226d50dfce3c6f
ASHOK KUMAR <96563901+ASHOK44321@users.noreply.github.com>
2022-05-07 11:35:27 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int max=a%10;     while (a>0)     {         int r=a%10;         if (max<r)         {             max=r;             }             a=a/10;             }             printf("%d",max);             return 0;
    }

---
+++ Largest_Digit.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{ 
+    int a;
+    scanf("%d",&a);
+    int max=a%10;
+    while (a>0) 
+    { 
+        int r=a%10;
+        if (max<r)
+        {
+            max=r;
+            }
+            a=a/10;
+            } 
+            printf("%d",max);
+            return 0;
+    
+}
