252a5c05f1563991a08ef12617cc80a88882aeb5
ASHOK KUMAR <96563901+ASHOK44321@users.noreply.github.com>
2022-05-07 11:24:11 +0530

    #include<stdio.h> int main() {     int k;     scanf("%d",&k);     for (int z=0; z<k; z++)     {         int a,b;         scanf("%d%d",&a,&b);         printf("%d ",a+b);         }         return 0;
    }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int k;
+    scanf("%d",&k); 
+    for (int z=0; z<k; z++)
+    { 
+        int a,b;
+        scanf("%d%d",&a,&b);
+        printf("%d
+",a+b);
+        }
+        return 0;
+    
+}
