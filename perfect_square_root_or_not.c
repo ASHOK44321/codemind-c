4b1c0ff11d5feda156cde24108c45cbb11cedea4
ASHOK KUMAR <96563901+ASHOK44321@users.noreply.github.com>
2022-05-07 11:28:42 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int i,flag=0;     for (i=0; i<a; i++)     {         if (i*i==a)         {             flag=1;             break;             }             }             if (flag==1)             {                 printf("True");                 }                 else                 {                     printf("False");                     }                     return 0;
    }

---
+++ perfect_square_root_or_not.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{ 
+    int a;
+    scanf("%d",&a); 
+    int i,flag=0;
+    for (i=0; i<a; i++)
+    {
+        if (i*i==a) 
+        {
+            flag=1;
+            break;
+            }
+            }
+            if (flag==1)
+            { 
+                printf("True");
+                }
+                else
+                {
+                    printf("False");
+                    }
+                    return 0;
+    
+}
